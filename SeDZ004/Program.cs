7c6571e29ec793ca7a91bfdc9d304b2991dade8e
PavelPoz <79132207654@yandex.ru>
2022-11-12 17:36:24 +0700

    Решил Четвертую  задачу в ДЗ

---
+++ SeDZ004/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+
+*/
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num - 1;
+while (num1 > 0)
+{
+    if (num1 % 2 == 0)
+    {
+        Console.WriteLine(num1);
+        num1 = num1 -1;
+    }
+    else
+    {
+        num1 = num1 -1;
+    }
+}
