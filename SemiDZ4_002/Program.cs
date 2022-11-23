f525cf887c4530874d47b26d7b04e18efb85e1f6
PavelPoze <79132207654@yandex.ru>
2022-11-23 10:31:39 +0700

    Решение второй задачи 4го семинара

---
+++ SemiDZ4_002/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12  */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
+SumNumbers(number, count);
+void SumNumbers(int n, int len)   // Функция вывода суммы цифр в числе
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+
