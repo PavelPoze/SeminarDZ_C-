8d9273b04b93557e70dc27d5ac63d44cd7d987f8
PavelPoze <79132207654@yandex.ru>
2022-11-23 10:30:30 +0700

    Решение первой задачи 4го семинара

---
+++ SemiDZ4_001/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+Console.WriteLine("введите число 1 ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2 ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sum = numberA;
+int i = 1;
+while (i < numberB)
+{
+    sum = sum * numberA;
+    i++;
+}
+Console.WriteLine(sum);
