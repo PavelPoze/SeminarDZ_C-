d6c6ac11d1f7a93eb5211a825eea59dcae7ba41e
PavelPoze <79132207654@yandex.ru>
2022-11-23 10:32:17 +0700

    Решение третьей задачи 4го семинара

---
+++ SemiDZ4_003/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+ Вывод сделать отдельным методом.
+5 -> [1, 2, 5, 7, 19] 
+3 -> [6, 1, 33]   */
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
+
+int[] randomArray = new int[number];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+
