5d8f6af1e44d6151b032aea5661c54cee41250c1
PavelPoze <79132207654@yandex.ru>
2022-12-04 08:19:33 +0700

    Решение третьей задачи

---
+++ SemiDZ7_03/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задача 52: Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+int m = ReadInt("Введите число строк: ");     
+int n = ReadInt("Введите число стоблбцов: ");
+int[,] number = new int[m,n];   // создаем двумерный массив исходя из введенных данных
+FillMatrix(number);
+WriteMatrix(number);
+for (int j = 0; j < number.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < number.GetLength(0); i++)
+    {
+        avarage = (avarage + number[i, j]);
+    }
+    avarage = avarage / m;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+
+void FillMatrix(int[,] array)     //заполнение двумерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)   // цикл для заполнения строк
+    {
+        for (int j = 0; j < array.GetLength(1); j++)   // цикл для заполнения стодбцов
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void WriteMatrix(int[,] array)    // вывод двумерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)   // цикл для заполнения строк
+    {
+        for (int j = 0; j <array.GetLength(1); j++)   // цикл для заполнения стодбцов
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
